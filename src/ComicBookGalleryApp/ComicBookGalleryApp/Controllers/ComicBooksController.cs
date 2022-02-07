544ab1fdc764fd4029c8242a37970aaf73c3f1bd
Benjamin Pryor <bscott23@gmail.com>
2022-02-07 13:04:39 -0500

    Added Detail Controller

---
+++ src/ComicBookGalleryApp/ComicBookGalleryApp/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGalleryApp.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public string Detail ()
+        {
+            return "Hello from the comic books controller!";
+        }
+    }
+}
