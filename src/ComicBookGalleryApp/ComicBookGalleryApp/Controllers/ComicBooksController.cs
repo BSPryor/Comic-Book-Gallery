1a2b14e9a83781c58ea793f43fcba47da41e8c39
Benjamin Pryor <bscott23@gmail.com>
2022-02-07 14:33:38 -0500

    Updated Detail Action Method

--- src/ComicBookGalleryApp/ComicBookGalleryApp/Controllers/ComicBooksController.cs
+++ src/ComicBookGalleryApp/ComicBookGalleryApp/Controllers/ComicBooksController.cs
@@ -4,9 +4,13 @@ namespace ComicBookGalleryApp.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail ()
+        public ActionResult Detail ()
         {
-            return "Hello from the comic books controller!";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            };
+            return Content("Hello from the comic books controller!");
         }
     }
 }
