d731991d54ab334b95e02a0f91c518103f60ce37
Benjamin Pryor <bscott23@gmail.com>
2022-02-08 12:49:21 -0500

    Updated comicbook model view to be strongly typed

--- src/ComicBookGalleryApp/ComicBookGalleryApp/Controllers/ComicBooksController.cs
+++ src/ComicBookGalleryApp/ComicBookGalleryApp/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComicBookGalleryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComicBookGalleryApp.Controllers
 {
@@ -6,18 +7,24 @@ namespace ComicBookGalleryApp.Controllers
     {
         public ActionResult Detail ()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-man!";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-                    {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-                    };
-            return View();
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-man!",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
+                }
+        };
+            
+
+            
+            return View(comicBook);
         }
     }
 }
